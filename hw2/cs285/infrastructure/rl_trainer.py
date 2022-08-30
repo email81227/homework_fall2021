d6551b8417b210c1428fa045f383c65e91e528f5
ding <email81227@gmail.com>
2022-08-31 03:12:42 +0800

    complete hw2 rl_Trainer.py

--- hw2/cs285/infrastructure/rl_trainer.py
+++ hw2/cs285/infrastructure/rl_trainer.py
@@ -165,38 +165,38 @@ class RL_Trainer(object):
             envsteps_this_batch: the sum over the numbers of environment steps in paths
             train_video_paths: paths which also contain videos for visualization purposes
         """
-        # TODO: GETTHIS from HW1
-        # TODO decide whether to load training data or use the current policy to collect more data
+        use_expertdata = itr == 0
+        # ---- GET THIS from HW1
+        # ---- decide whether to load training data or use the current policy to collect more data
         # HINT: depending on if it's the first iteration or not, decide whether to either
                 # (1) load the data. In this case you can directly return as follows
                 # ``` return loaded_paths, 0, None ```
 
                 # (2) collect `self.params['batch_size']` transitions
 
-        if iter == 0:
-            loaded_paths = load(initial_expertdata)
-
-            return loaded_paths, 0, None
+        if use_expertdata:
+            with open(initial_expertdata, "rb") as f:
+                paths = pickle.load(f)
+            envsteps_this_batch = 0
         else:
-
-        # TODO collect `batch_size` samples to be used for training
-        # HINT1: use sample_trajectories from utils
-        # HINT2: you want each of these collected rollouts to be of length self.params['ep_len']
-        print("\nCollecting data to be used for training...")
-        # TODO (V)
-        paths, envsteps_this_batch = utils.sample_trajectories(
-            env=self.env,
-            policy=collect_policy,
-            min_timesteps_per_batch=self.env.model.opt.timestep,
-            max_path_length=self.params['ep_len'],
-        )
+            # ---- collect `batch_size` samples to be used for training
+            # HINT1: use sample_trajectories from utils
+            # HINT2: you want each of these collected rollouts to be of length self.params['ep_len']
+            print("\nCollecting data to be used for training...")
+            # ---- (V)
+            paths, envsteps_this_batch = utils.sample_trajectories(
+                env=self.env,
+                policy=collect_policy,
+                min_timesteps_per_batch=self.params['batch_size'],
+                max_path_length=self.params['ep_len'],
+            )
 
         # collect more rollouts with the same policy, to be saved as videos in tensorboard
         # note: here, we collect MAX_NVIDEO rollouts, each of length MAX_VIDEO_LEN
         train_video_paths = None
-        if self.log_video:
+        if self.logvideo:
             print('\nCollecting train rollouts to be used for saving videos...')
-            ## TODO look in utils and implement sample_n_trajectories
+            ## ---- look in utils and implement sample_n_trajectories
             train_video_paths = utils.sample_n_trajectories(
                 self.env, collect_policy, MAX_NVIDEO, MAX_VIDEO_LEN, True
             )
@@ -204,7 +204,29 @@ class RL_Trainer(object):
         return paths, envsteps_this_batch, train_video_paths
 
     def train_agent(self):
-        # TODO: GETTHIS from HW1
+        # ---- GET THIS from HW1
+        print('\nTraining agent using sampled data from replay buffer...')
+        all_logs = []
+        for train_step in range(self.params['num_agent_train_steps_per_iter']):
+
+            # XXX sample some data from the data buffer
+            # HINT1: use the agent's sample function
+            # HINT2: how much data = self.params['train_batch_size']
+            ob_batch, ac_batch, re_batch, next_ob_batch, terminal_batch = \
+                self.agent.sample(self.params['train_batch_size'])
+
+            # XXX use the sampled data to train an agent
+            # HINT: use the agent's train function
+            # HINT: keep the agent's training log for debugging
+            train_log = self.agent.train(
+                ob_batch,
+                ac_batch,
+                re_batch,
+                next_ob_batch,
+                terminal_batch,
+            )
+            all_logs.append(train_log)
+        return all_logs
 
     ####################################
     ####################################
